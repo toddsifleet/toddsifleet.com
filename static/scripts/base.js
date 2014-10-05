6ab6707f7e476ff837b3664774c1412498866937
Todd Sifleet <todd.sifleet@gmail.com>
2014-10-05 18:47:18 -0400

    update site

--- static/scripts/base.js
+++ static/scripts/base.js
@@ -1,15 +1,1 @@
-function show_navigation() {
-  var title = "{{ page.title }}";
-  var navigation = document.getElementById("nav-menu");
-  var navigation_button = document.getElementById("mobile-nav-button");
-  var height = navigation.style.getPropertyValue('max-height');
-  if (height == '0px' || height == null) {
-    navigation.style.setProperty('max-height', {{navigation_links|length}} * 70 + 'px');
-    navigation_button.innerHTML = "- " + title + " -";
-  }
-  else {
-    navigation.style.setProperty('max-height', '0px');
-    navigation_button.innerHTML = "+ " + title + " +";
-  }
-}
 
