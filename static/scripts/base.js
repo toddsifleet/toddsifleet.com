c02ead54b9f89635c6114490c664f3f6669305e9
Todd Sifleet <todd.sifleet@gmail.com>
2014-11-01 12:22:07 -0700

    use hamburger button for mobile nav bar

--- static/scripts/base.js
+++ static/scripts/base.js
@@ -1,6 +1,5 @@
 var navController = (function() {
   var navigation = document.getElementById("nav-menu");
-  var title = navigation.dataset.pageTitle;
   var navigation_button = document.getElementById("mobile-nav-button");
   var nav_link_count = navigation.querySelectorAll('li').length;
 
@@ -9,11 +8,9 @@ var navController = (function() {
       var height = navigation.style.getPropertyValue('max-height');
       if (height == '0px' || height == null) {
         navigation.style.setProperty('max-height', nav_link_count * 70 + 'px');
-        navigation_button.innerHTML = "- " + title + " -";
       }
       else {
         navigation.style.setProperty('max-height', '0px');
-        navigation_button.innerHTML = "+ " + title + " +";
       }
     }
   };
